0857b5716df160a4aadf2e8d827cfcc41d35b4ec
Jecheon Ryu <rjc1704@gmail.com>
2022-12-09 18:01:24 +0900

    feat: UI-1 작업완료

--- App.js
+++ App.js
@@ -1,20 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.safearea}>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <View style={styles.tabs}>
+          <View style={styles.tab}>
+            <Text style={styles.tabText}>Javascript</Text>
+          </View>
+          <View style={{ ...styles.tab, backgroundColor: "grey" }}>
+            <Text style={styles.tabText}>React</Text>
+          </View>
+          <View style={{ ...styles.tab, backgroundColor: "grey" }}>
+            <Text style={styles.tabText}>Coding Test</Text>
+          </View>
+        </View>
+        <View style={styles.inputWrapper}>
+          <TextInput placeholder="Enter your task" style={styles.input} />
+        </View>
+        <ScrollView>
+          <View style={styles.task}>
+            <Text>신나는 실행컨텍스트 공부</Text>
+            <View style={{ flexDirection: "row" }}>
+              <AntDesign name="checksquare" size={24} color="black" />
+              <Feather
+                style={{ marginLeft: 10 }}
+                name="edit"
+                size={24}
+                color="black"
+              />
+              <AntDesign
+                style={{ marginLeft: 10 }}
+                name="delete"
+                size={24}
+                color="black"
+              />
+            </View>
+          </View>
+          <View style={styles.task}>
+            <Text>너무 좋은 ES6 최신문법 공부</Text>
+            <View style={{ flexDirection: "row" }}>
+              <AntDesign name="checksquare" size={24} color="black" />
+              <Feather
+                style={{ marginLeft: 10 }}
+                name="edit"
+                size={24}
+                color="black"
+              />
+              <AntDesign
+                style={{ marginLeft: 10 }}
+                name="delete"
+                size={24}
+                color="black"
+              />
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safearea: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+  },
+  tabs: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  tab: {
+    backgroundColor: "#0FBCF9",
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+    width: "30%",
+    alignItems: "center",
+  },
+  tabText: {
+    fontWeight: "600",
+  },
+  inputWrapper: {
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    paddingVertical: 15,
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  input: {
+    borderWidth: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  task: {
+    flexDirection: "row",
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    backgroundColor: "#D9D9D9",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10,
   },
 });

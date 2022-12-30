6b29a6939829eed7145e12fe3fc08a99b0cc9140
Jecheon Ryu <rjc1704@gmail.com>
2022-12-30 15:18:49 +0900

    라이브코딩 AsyncStorage까지

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import {
+  Alert,
   SafeAreaView,
   ScrollView,
   StyleSheet,
@@ -10,14 +11,16 @@ import {
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  // Set Done
-  // 완료 토글링
+  // delete todo
+  // 삭제 이모티콘 터치 시 해당 todo 삭제
   const [todos, setTodos] = useState([]);
-  const [category, setCategory] = useState("js"); // js, react, ct
+  const [category, setCategory] = useState(""); // js, react, ct
   const [text, setText] = useState("");
+  const [editText, setEditText] = useState("");
 
   const newTodo = {
     id: Date.now(),
@@ -32,6 +35,43 @@ export default function App() {
     setText("");
   };
 
+  const deleteTodo = (id) => {
+    // 1. id 값을 받아서 해당 배열 요소를 제외한 나머지를 새로운 배열로 받는다.
+    // 2. setTodos
+    Alert.alert("Todo 삭제", "정말 삭제하시겠습니까?", [
+      {
+        text: "취소",
+        style: "cancel",
+        onPress: () => console.log("취소 클릭!"),
+      },
+      {
+        text: "삭제",
+        style: "destructive",
+        onPress: () => {
+          const newTodos = todos.filter((todo) => todo.id !== id);
+          setTodos(newTodos);
+        },
+      },
+    ]);
+  };
+
+  const setEdit = (id) => {
+    const newTodos = [...todos];
+    const idx = newTodos.findIndex((todo) => todo.id === id);
+    newTodos[idx].isEdit = !newTodos[idx].isEdit;
+    setTodos(newTodos);
+  };
+
+  const editTodo = (id) => {
+    // 1. id 값받아서 해당 배열의 요소를 찾는다. idx 찾기
+    // 2. todos[idx].text = editText;
+    const newTodos = [...todos];
+    const idx = newTodos.findIndex((todo) => todo.id === id);
+    newTodos[idx].text = editText;
+    newTodos[idx].isEdit = false;
+    setTodos(newTodos);
+  };
+
   const setDone = (id) => {
     // 1. id를 매개변수로 받는다.
     // 2. id에 해당하는 배열의 요소를 찾는다.
@@ -42,13 +82,38 @@ export default function App() {
     setTodos(newTodos);
   };
 
+  const setCat = async (cat) => {
+    console.log("cat:", cat);
+    setCategory(cat);
+    await AsyncStorage.setItem("category", cat);
+  };
+
+  useEffect(() => {
+    // 현재의 최신 todos를 AsyncStorage에 저장
+    const saveTodos = async () => {
+      await AsyncStorage.setItem("todos", JSON.stringify(todos));
+    };
+    if (todos.length > 0) saveTodos();
+  }, [todos]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const resp_todos = await AsyncStorage.getItem("todos"); // todos 배열
+      const resp_cat = await AsyncStorage.getItem("category"); // undefined / null
+
+      setTodos(JSON.parse(resp_todos));
+      setCategory(resp_cat ?? "js");
+    };
+    getData();
+  }, []);
+
   return (
     <SafeAreaView style={styles.safearea}>
       <StatusBar style="auto" />
       <View style={styles.container}>
         <View style={styles.tabs}>
           <TouchableOpacity
-            onPress={() => setCategory("js")}
+            onPress={() => setCat("js")}
             style={{
               ...styles.tab,
               backgroundColor: category === "js" ? "#0FBCF9" : "grey",
@@ -57,7 +122,7 @@ export default function App() {
             <Text style={styles.tabText}>Javascript</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() => setCategory("react")}
+            onPress={() => setCat("react")}
             style={{
               ...styles.tab,
               backgroundColor: category === "react" ? "#0FBCF9" : "grey",
@@ -66,7 +131,7 @@ export default function App() {
             <Text style={styles.tabText}>React</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            onPress={() => setCategory("ct")}
+            onPress={() => setCat("ct")}
             style={{
               ...styles.tab,
               backgroundColor: category === "ct" ? "#0FBCF9" : "grey",
@@ -89,29 +154,45 @@ export default function App() {
             if (category === todo.category) {
               return (
                 <View key={todo.id} style={styles.task}>
-                  <Text
-                    style={{
-                      textDecorationLine: todo.isDone ? "line-through" : "none",
-                    }}
-                  >
-                    {todo.text}
-                  </Text>
+                  {todo.isEdit ? (
+                    <TextInput
+                      onSubmitEditing={() => editTodo(todo.id)}
+                      onChangeText={setEditText}
+                      value={editText}
+                      style={{ backgroundColor: "white", flex: 1 }}
+                    />
+                  ) : (
+                    <Text
+                      style={{
+                        textDecorationLine: todo.isDone
+                          ? "line-through"
+                          : "none",
+                      }}
+                    >
+                      {todo.text}
+                    </Text>
+                  )}
+
                   <View style={{ flexDirection: "row" }}>
                     <TouchableOpacity onPress={() => setDone(todo.id)}>
                       <AntDesign name="checksquare" size={24} color="black" />
                     </TouchableOpacity>
-                    <Feather
-                      style={{ marginLeft: 10 }}
-                      name="edit"
-                      size={24}
-                      color="black"
-                    />
-                    <AntDesign
-                      style={{ marginLeft: 10 }}
-                      name="delete"
-                      size={24}
-                      color="black"
-                    />
+                    <TouchableOpacity onPress={() => setEdit(todo.id)}>
+                      <Feather
+                        style={{ marginLeft: 10 }}
+                        name="edit"
+                        size={24}
+                        color="black"
+                      />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => deleteTodo(todo.id)}>
+                      <AntDesign
+                        style={{ marginLeft: 10 }}
+                        name="delete"
+                        size={24}
+                        color="black"
+                      />
+                    </TouchableOpacity>
                   </View>
                 </View>
               );
